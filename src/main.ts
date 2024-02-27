cd3c702e14b36c6e414d4179a4e6797f5354cf01
irankk <rossovr00@mail.ru>
2024-02-27 20:52:00 +0300

    123

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,11 @@
 import './style.css'
+console.log("hhh")
+
+//console.log(typeof 12) 'number'
+// let year = 2010
+// if (year==2015) {
+//   console.log('yes')
+// } else {
+//   console.log('no')
+// }
 
