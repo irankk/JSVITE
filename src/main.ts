05af7d3b753b01b66510177139a1ae206895d979
irankk <rossovr00@mail.ru>
2024-02-29 21:07:14 +0300

    342

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,314 @@
-import './style.css'
-//console.log("hhh")
+import './style.scss'
+console.log('Hello world')
+document.write('<div id="main" style="background: lightblue;">dfgdfgdf</div>')
 
-//console.log(typeof 12) 'number'
-// let year = 2010
-// if (year==2015) {
-//   console.log('yes')
-// } else {
-//   console.log('no')
+a = 25
+
+var a = 0
+console.log(a)
+var a = 2
+console.log(a)
+
+// cоздание переменной
+// let newVar = 5
+// newVar = 15
+
+// let noVal
+// noVal = 'g'
+// console.log(noVal)
+
+// let theValue
+
+// cоздание константы
+// const DAY_IN_WEEK = 7
+// Ошибка не можем изменять константы
+// DAY_IN_WEEK = 8
+
+// const countedSum = a + newVar
+
+console.log('10 + 5 =', 10+5)
+console.log('10 - 5 =', 10-5)
+console.log('10 * 5 =', 10*5)
+console.log('10 / 5 =', 10/5)
+console.log('10 ** 5 =', 10**5) //возведение в степень
+console.log('10 % 5 =', 10%5)   //остаток от деления
+console.log('3 % 2 =', 3%2)   //остаток от деления
+
+// let userInput = prompt('Введите число', 10)
+// console.log(userInput)
+// console.log(typeof userInput)
+// userInput = parseInt(userInput)
+// console.log(userInput)
+// console.log(typeof userInput)
+
+
+console.log('456')
+console.log(456)
+
+console.log((5-5%2)/2)
+
+// alert('Вы ввели ' + userInput)
+
+
+// Практика 1. Задание 1
+// Запросите у пользователя число, возведите это число во 2 - ю степень и выведите на экран.
+// let num = parseInt(prompt('Введите число',4))
+// alert ('Ваше число, возведённое во 2 степень' + num**2)
+
+console.log(typeof 12)
+console.log(typeof 12.6)
+console.log(typeof (12.6/0))
+console.log(typeof 12n)
+console.log(12.6/0)
+console.log(parseInt('a'))
+console.log(parseInt('12.5adasd'))
+console.log(parseFloat('12.5adasd'))
+console.log(+'12.5adasd')
+console.log(+'12.5')
+console.log(Number('12.5'))
+console.log(String(12.5))
+console.log(12.5.toString())
+console.log(typeof '12')
+console.log(typeof true)
+console.log(typeof false)
+console.log(typeof [])
+console.log(typeof {})
+console.log(typeof undefined)
+let variable
+console.log(variable)
+console.log(null)
+console.log(typeof null)
+console.log(typeof alert)
+console.log(typeof '')
+// console.log(`В этой строке мы сложим 2 числа ${newVar + 5}. Вот так`)
+// console.log('В этой строке мы сложим 2 числа ' + (newVar+5) + '. Вот так')
+
+// В JavaScript есть 8 основных типов данных.
+
+// Семь из них называют «примитивными» типами данных:
+// number для любых чисел: целочисленных или чисел с плавающей точкой; целочисленные значения ограничены диапазоном ±(253 - 1).
+// bigint для целых чисел произвольной длины.
+// string для строк.Строка может содержать ноль или больше символов, нет отдельного символьного типа.
+// boolean для true / false.
+// null для неизвестных значений – отдельный тип, имеющий одно значение null.
+// undefined для неприсвоенных значений – отдельный тип, имеющий одно значение undefined.
+// symbol для уникальных идентификаторов.
+// И один не является «примитивным» и стоит особняком:
+// object для более сложных структур данных.
+// Оператор typeof позволяет нам увидеть, какой тип данных сохранён в переменной.
+
+// Имеет две формы: typeof x или typeof (x).
+// Возвращает строку с именем типа.Например, "string".
+// Для null возвращается "object" – это ошибка в языке, на самом деле это не объект.
+// В следующих главах мы сконцентрируемся на примитивных значениях, а когда познакомимся с ними, перейдём к объектам.
+
+let num1 = 1
+let num2 = 2
+console.log(2 > 1)
+console.log(num1 == num2)
+console.log(num1 != num2)
+console.log(num1 != 1)
+console.log(num1 == 1)
+console.log(num1 === 1)
+console.log(1 === +'1')
+
+console.log(+false)
+console.log(+true)
+
+// Все значения, которы станут false при преобразовании в булев тип
+console.log(Boolean(''))
+console.log(Boolean(0))
+console.log(Boolean(undefined))
+console.log(Boolean(null))
+console.log(Boolean(NaN))
+console.log(Boolean(false))
+
+console.log('z' > 'Z')
+
+console.log(null === undefined)
+console.log(null == undefined)
+console.log(null == null)
+
+console.log(isNaN(NaN))
+console.log(isNaN(1))
+
+// Операторы сравнения возвращают значения логического типа.
+// Строки сравниваются посимвольно в лексикографическом порядке.
+// Значения разных типов при сравнении приводятся к числу.Исключением является сравнение с помощью операторов строгого равенства / неравенства.
+// Значения null и undefined равны == друг другу и не равны любому другому значению.
+// Будьте осторожны при использовании операторов сравнений вроде > и < с переменными, которые могут принимать значения null / undefined.Хорошей идеей будет сделать отдельную проверку на null / undefined.
+
+let year = 2015
+let month = 1
+
+if (year == 2015) {
+  console.log('yes')
+}
+
+if (year==2015) {
+  console.log('yes')
+} else {
+  if (month==1) {
+    console.log('mo==1')
+  }
+  console.log('no')
+}
+
+if (year==2015) {
+  console.log('yes')
+} else if (year==2010) {
+  console.log('maybe')
+} else {
+  console.log('no')
+}
+
+let age = 18
+let message
+if (age>16) {
+  message='hello'
+} else {
+  message='too yang'
+}
+console.log(message)
+
+
+// переменная = (условие) ? Значение, которое присвоится в случае истинности условия : Значение, которое присвоится в случае ложности условия
+message = age > 16 ? 'hello' : 'too yang'
+
+message = (age < 3) ? 'Здравствуй, малыш!' :
+          (age < 18) ? 'Привет!' :
+          (age < 100) ? 'Здравствуйте!' : 'Какой необычный возраст!'
+
+if (age<3) {
+  message = 'Здравствуй, малыш!'
+} else {
+  if (age < 18) {
+    message = 'Привет!'
+  } else {
+    if (age < 100) {
+      message = 'Здравствуйте!'
+    } else {
+      message = 'Какой необычный возраст!'
+    }
+  }
+}
+
+const isThursday = new Date().getDate() == 22 ? true : false
+console.log(isThursday)
+
+let x = '1'
+switch (x) {
+  case '1':  // if (x === '1')
+    console.log('x=1')
+    break
+  case '2':  // if (x === '2')
+    console.log('x=2')
+    break
+  default:
+    console.log('x не 1 и не 2')
+}
+
+console.log(true || true)   
+console.log(false || true)  
+console.log(true || false)  
+console.log(false || false)
+
+// Оператор || выполняет следующие действия:
+
+// Вычисляет операнды слева направо.
+// Каждый операнд конвертирует в логическое значение.Если результат true, останавливается и возвращает исходное значение этого операнда.
+// Если все операнды являются ложными(false), возвращает последний из них.
+console.log(0 || 0 || 'gfdg' || 0)
+console.log(0 || 0 || 0 || null)
+
+let aStr = ''
+let bStr = 'Default'
+
+aStr ||= bStr
+aStr = aStr ? aStr : bStr
+console.log(aStr)
+
+console.log(true && true)
+console.log(false && true)
+console.log(true && false)
+console.log(false && false)
+
+// Оператор && выполняет следующие действия:
+
+// Вычисляет операнды слева направо.
+// Каждый операнд преобразует в логическое значение.Если результат false, останавливается и возвращает исходное значение этого операнда.
+// Если все операнды были истинными, возвращается последний.
+// Другими словами, И возвращает первое ложное значение.Или последнее, если ничего не найдено.
+console.log(1 && 11 && 'gfdg' && 'sdf')
+console.log('' && 0 && 0 && null)
+
+aStr = 'Hello'
+
+aStr &&= aStr + ' user'
+aStr = aStr ? aStr + '!' : aStr
+console.log(aStr)
+
+// Оператор ! принимает один аргумент и выполняет следующие действия:
+
+// Сначала приводит аргумент к логическому типу true / false.
+// Затем возвращает противоположное значение.
+
+console.log(!true)
+console.log(!false)
+console.log(!!1)
+console.log(Boolean(1))
+console.log(!!0)
+console.log(Boolean(0))
+
+
+// while (условие) {
+   // код
+   // также называемый "телом цикла"
 // }
 
-function factorial(num: number) : number{
+// Цикл с предусловием
+let i = 0
+while (i<5) {
+  i++
+  console.log(i)
+}
+
+// Цикл с постусловием
+i = 0
+do {
+  i++
+  console.log(i)
+} while(i<0)
+
+// for (начало; условие; шаг) {
+  // ... тело цикла ...
+  // }
+  
+for (let i=3; i < 5; i++ ) {
+  if (i==3) continue
+  console.log(i)
+  break
+}
+
+// break - выход из цикла
+// continue - переход на следующую итерацию без выполнения оставшегося тела цикла
+
+//  0, 1 
+//  2, 3
+
+// [ [0,1],
+//   [2,3] ]
+
+for (let x=0; x<2; x++) {
+  for (let y=0; y<2; y++) {
+    console.log(`(x:${x}, y:${y})`)
+  }
+}
+
+function fact(num: number) : number{
   if (num==0) return 1
-  else return num * factorial(num - 1)
+  else return num * fact(num - 1)
 }
 
 console.log('factorial 5 is:')
-console.log(factorial(5))
+console.log(fact(5))
